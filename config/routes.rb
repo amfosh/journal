9c8478f072e000dff1962a865ab283cbad8d6725
amfosh <afasciocco@gmail.com>
2021-05-01 10:49:37 -0400

    re-routed login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
+
